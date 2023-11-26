dd9a45da0a36deeca3dc893704fb96bb4ffb4b87
RaelMartinss <raelpgm2@gmail.com>
2023-11-25 23:14:20 -0300

    Chore: plugins

--- src/app/components/NavLinks.tsx
+++ src/app/components/NavLinks.tsx
@@ -4,7 +4,7 @@ export const NavLinks = () => (
     <nav>
         <ul className="hidden md:flex md:space-x-4">
             <li>Home</li>
-            <li>TV Shows</li>''
+            <li>TV Shows</li>
             <li>Movies</li>
             <li>Latest</li>
         </ul>
