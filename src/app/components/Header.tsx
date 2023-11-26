cd5154bfafe97ecf9abf8b9d451de84c271348ee
RaelMartinss <raelpgm2@gmail.com>
2023-11-26 00:20:01 -0300

    Chore: Adding Search

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,48 @@
 'use client'
-import React from "react";
+import React, { useState } from "react";
 import { UserProfile } from "./UserProfile";
 import { Logo } from "./Logo";
 import { NavLinks } from "./NavLinks";
 import { useScroll } from "../hooks/useScroll";
+import { SearchForm } from './SearchForm'
+import Link from 'next/link';
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Header() {
 
     const isScrolled = useScroll();
+    const router = useRouter();
+    const params = useSearchParams();
+    const initialSearchTerm = params.get('title') || '';
+    const [searchTerm, setSearchTerm] = useState<string>(initialSearchTerm);
+  
+    const onSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchTerm(event.target.value);
+    };
+  
+    const onSearch = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const newParams = new URLSearchParams(params.toString());
+      newParams.set('title', searchTerm);
+      router.push(`/search?${newParams.toString()}`);
+    };
 
     return (
         <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-4 lg:py-2`}>
             <div className="flex items-center space-x-2 md:space-x-8">
-                <Logo />
+                <Link href='/'>
+                    <Logo />
+                </Link>
                 <NavLinks />
             </div>
-            <UserProfile/>
+            <div className="flex items-center space-x-2 md:space-x-8">
+                <SearchForm
+                    onSearch={onSearch}
+                    searchTerm={searchTerm}
+                    onSearchTermChange={onSearchTermChange}
+                />
+                <UserProfile/>
+            </div>
 
         </header>
     )
