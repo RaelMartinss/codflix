c36a30d3dc6072be1a3265d8c110a73fd806faf2
RaelMartinss <raelpgm2@gmail.com>
2023-11-25 21:19:14 -0300

    Chore: adding header transitions

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 
 export default function Header() {
+    const [ isScrolled, setIsScrolled] = useState(false);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true)
+            }else {
+                setIsScrolled(false)
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    })
     return (
-        <header className='fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6'>
+        <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-4 lg:py-2`}>
             {/* <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix" width={120} height={120}/> */}
             <div className="flex items-center space-x-2 md:space-x-8">
                 <img src="/logowhite.png" alt="logo" width={120} height={120} />
