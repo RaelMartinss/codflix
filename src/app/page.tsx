ff44c27662199b7ea4ea58d3f879db2525efcb1c
RaelMartinss <raelpgm2@gmail.com>
2024-02-29 23:45:37 -0300

    api request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
 import { Banner } from './components/Banner';
+import { getFeaturedMovie } from './auth/server/MovieService';
 
 
-export default function Home() {
+export default async function Home() {
+  const featuredMovie = await getFeaturedMovie('106')
   return (
     <div className='relative bg-gradient-to-b pb-8'>
       <Header /> 
       <main className="relative overflow-y-scroll p-8 pb-20 scrollbar-hide lg:px-16 ">
-        <Banner />
+        <Banner movie={featuredMovie} />
         <MovieRow sectionTitle='Trending Now' />
         <MovieRow sectionTitle='Top Rated'/>
         <MovieRow sectionTitle='Action Movies'/>
